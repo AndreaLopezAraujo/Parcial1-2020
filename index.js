7851ad545e0c2941fcc0a9254bf17cd1e16fea41
AndreaLope <ac.lopez@uniandes.edu.co>
2020-09-23 02:02:50 -0500

    arreglos

--- index.js
+++ index.js
@@ -2,48 +2,70 @@ url =
     "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json";
 
 const info = document.getElementById("info");
+const totalCarrito = document.getElementById("numero");
+let total = "";
+numero = 0;
 valor = "Burguers";
 function changeContent(valor) {
-    info.innerHTML ="";
+    info.innerHTML = "";
+    totalCarrito.innerHTML = numero + " items";
     fetch(url)
         .then((response) => response.json())
         .then((json) => {
-            let titulo=`<h2 align="center"><hr>${valor}<hr></h2>`;
-            let total="";
-            let ini= `<div class="card-deck">`;
-            let fin=`</div>`;
+            let titulo = `<h2 align="center"><hr>${valor}<hr></h2>`;
+            let ini = `<div class="card-deck">`;
+            let fin = `</div>`;
             let comidas;
             i = 0;
             if (valor == "Tacos") {
                 i = 1;
+                comidas = json[i].products;
+                Concomidas(comidas);
+                info.innerHTML += titulo + ini + total + fin;
             }
             else if (valor == "Salads") {
                 i = 2;
+                comidas = json[i].products;
+                Concomidas(comidas);
+                info.innerHTML += titulo + ini + total + fin;
+            }
+            else if (valor == "Burguers") {
+                i = 0;
+                comidas = json[i].products;
+                Concomidas(comidas);
+                info.innerHTML += titulo + ini + total + fin;
             }
             else if (valor == "Desserts") {
                 i = 3;
+                comidas = json[i].products;
+                Concomidas(comidas);
+                info.innerHTML += titulo + ini + total + fin;
             }
             else if (valor == "Drinks and Sides") {
                 i = 4;
+                comidas = json[i].products;
+                Concomidas(comidas);
+                info.innerHTML += titulo + ini + total + fin;
             }
-            comidas = json[i].products;
-            for (let o of comidas) {
-                let content = `<div class="card mb-3" style="min-width: 15rem;">\
-        <img src="${o["image"]}" class="card-img-top" alt="${o["name"]}">\
-        <div class="card-body">\
-          <h5 class="card-title">${o["name"]}</h5>\
-          <p class="card-text">${o["description"]}</p>\
-          <p class="card-text">$${o["price"]}</p>\
-          <button type="button" class="btn btn-dark">Add to car</button>
-        </div>\
-        </div>`;
-                total += content;
-            }
-            info.innerHTML+=titulo+ini+total+fin;
         });
     console.log(fetch(url))
 }
-
+function Concomidas(comidas) {
+    info.innerHTML="";
+    total="";
+    for (let o of comidas) {
+        let content = `<div class="card mb-3" style="min-width: 15rem;">\
+<img src="${o["image"]}" class="card-img-top" alt="${o["name"]}">\
+<div class="card-body">\
+  <h5 class="card-title">${o["name"]}</h5>\
+  <p class="card-text">${o["description"]}</p>\
+  <p class="card-text">$${o["price"]}</p>\
+  <button type="button" class="btn btn-dark">Add to car</button>
+</div>\
+</div>`;
+        total += content;
+    }
+}
 changeContent(valor)
 
 const p_burguers = document.getElementById("burguers_but")
@@ -69,5 +91,10 @@ p_desserts.addEventListener("click", () => {
 const p_drinks = document.getElementById("drinks_but")
 p_drinks.addEventListener("click", () => {
     valor = "Drinks and Sides";
+    changeContent(valor)
+})
+const p_carrito = document.getElementById("carrito_but")
+p_drinks.addEventListener("click", () => {
+    valor = "Carrito";
     changeContent(valor)
 })
